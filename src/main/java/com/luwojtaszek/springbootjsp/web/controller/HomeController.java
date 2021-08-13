a36a42274564b80384192557b2b89d97e248a9a5
preetas <88144614+preetas@users.noreply.github.com>
2021-08-13 16:30:11 +0530

    Update HomeController.java

--- src/main/java/com/luwojtaszek/springbootjsp/web/controller/HomeController.java
+++ src/main/java/com/luwojtaszek/springbootjsp/web/controller/HomeController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping("/index")
     public String getHomePage(Model model) {
         model.addAttribute("message", "Spring Boot application that uses JSP With Embedded Tomcat");
         return View.HOME.getPath();
