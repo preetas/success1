e3958d1d22eb08c1039df88548904bcc128938e4
preetas <88144614+preetas@users.noreply.github.com>
2021-08-13 19:16:16 +0530

    Update HomeController.java

--- src/main/java/com/luwojtaszek/springbootjsp/web/controller/HomeController.java
+++ src/main/java/com/luwojtaszek/springbootjsp/web/controller/HomeController.java
@@ -4,7 +4,7 @@ import com.luwojtaszek.springbootjsp.web.constant.View;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 /**
  * Created by lukasz on 27.08.2017.
  * With IntelliJ IDEA 15
