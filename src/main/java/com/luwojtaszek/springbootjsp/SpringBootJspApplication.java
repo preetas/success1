a3cdfd06e8ab31562072c4020befd987e8d854a5
preetas <88144614+preetas@users.noreply.github.com>
2021-08-13 16:53:12 +0530

    Update SpringBootJspApplication.java

--- src/main/java/com/luwojtaszek/springbootjsp/SpringBootJspApplication.java
+++ src/main/java/com/luwojtaszek/springbootjsp/SpringBootJspApplication.java
@@ -2,7 +2,8 @@ package com.luwojtaszek.springbootjsp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.ComponentScan;
+ 
 @SpringBootApplication
 @ComponentScan(basePackages= {"com.luwojtaszek"}
 public class SpringBootJspApplication {
